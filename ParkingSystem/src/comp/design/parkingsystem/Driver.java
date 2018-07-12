89de1f58c57ef6132b1bec86e91215c7162a6c4e
praja <praja@192.168.0.10>
2018-07-12 11:31:44 -0400

    Added attributes to driver class

--- ParkingSystem/src/comp/design/parkingsystem/Driver.java
+++ ParkingSystem/src/comp/design/parkingsystem/Driver.java
@@ -2,4 +2,12 @@ package comp.design.parkingsystem;
 
 public class Driver {
 
+	private String custName;
+	private String LicensePlate;
+	private int custId;
+	private int phoneNumber;
+	private String carMake;
+	private String carModel;
+	private int carYear;
+	private String custPassword;
 }
