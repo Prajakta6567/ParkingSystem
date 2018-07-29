fbcdd64907dbb546e847ac1bfc2234dbaaed4551
praja <praja@192.168.0.10>
2018-07-29 14:59:04 -0400

    Added JUnit Test case under TransactionTest

--- ParkingSystem/src/test/design/parkingsystem/TransactionTest.java
+++ ParkingSystem/src/test/design/parkingsystem/TransactionTest.java
@@ -6,13 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import comp.design.parkingsystem.Transaction;
-
-import java.util.ArrayList;
 import java.util.Calendar;
 
-public class TransactionTest {
-	Transaction transaction = null;
-	
+public class TransactionTest {	
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -24,12 +20,12 @@ public class TransactionTest {
 	@Test
 
 	public void getTimeDifferenceTest() {
-
-	Calendar calFirst;
-
-	Calendar calSecond;
-
-	transaction = new Transaction();
-
+	float actualValue=0;
+	Calendar oldYear = Calendar.getInstance();
+	oldYear.set(Calendar.YEAR, 2017);
+	Calendar newYear = Calendar.getInstance();
+	newYear.set(Calendar.YEAR, 2018);
+	actualValue=Transaction.getTimeDifference(newYear, oldYear);
+	        assertEquals(0.0, actualValue, 0.0001);
 	}
 }
